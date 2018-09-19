8423a82a5abcbf78a250ff821b1add3ec500956a
Vince Liem <vincepaul.liem@gmail.com>
2018-09-19 16:45:39 +0200

    finished

--- index.js
+++ index.js
@@ -2,34 +2,40 @@ const $ = require('jquery');
 const Game = require('./src/Game');
 
 const app = {
+
   game: new Game(),
-  board: this.emptyBoard(),
-  emptyBoard: function() {
-    return [...Array(3)].map(() => Array(3).fill(0));
-  },
+  board: [],
 
   init: function () {
     $( 'button#start' ).on('click', app.start);
+    app.board = app.game.emptyBoard();
     app.update();
-  },
-
-  start: function () {
-    app.game = new Game();
-    app.board = app.emptyBoard();
-    app.update();
-  },
-
-  add: function() {
-    $( 'p#info' ).html(app.game.play(app.board, [$(this).closest( 'tr' ).data('row'), $(this).data('col')]))
   },
 
   update: function () {
     $( 'table' ).html(app.game.display(app.board));
-    $( 'td' ).on('click', app.add)
-  }
+    $( 'table tr td' ).on('click', app.add)
+  },
+
+  start: function () {
+    app.game = new Game();
+    app.board = app.game.emptyBoard();
+    app.update();
+  },
+
+  add: function() {
+    const row = $(this).closest( 'tr' ).data('row'),
+          col = $(this).data('col');
+
+    if (!app.board[row][col]) {
+      app.board[row][col] = app.game.turn(app.board);
+      app.update();
+      $( 'p#info' ).html(app.game.win(app.board))
+    }
+  },
 
 };
 
 $(document).ready(function () {
-  app.init()
+  app.init();
 });
--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,44 @@
-module.exports.Game = class Game {
+module.exports = class Game {
   constructor() {
 
   }
 
   display(board) {
-    const displayRow = (acc, row, r) => acc + `<tr data-col="${r}">${row.reduce(displayCol, '')}</tr>`;
+    const displayRow = (acc, row, r) => acc + `<tr data-row="${r}">${row.reduce(displayCol, '')}</tr>`;
     const displayCol = (acc, col, c) => acc + `<td data-col="${c}" class="color-${col}"></td>`;
     return board.reduce(displayRow, '');
   }
 
-  play(board, coordinates) {
+  emptyBoard () {
+    return [...Array(3)].map(() => Array(3).fill(0));
+  }
 
+  turn(board) {
+    return board.toString().split(',').map(i => parseInt(i) === 2 ? -1 : parseInt(i)).reduce((acc, i) => acc + i, 0) ? 2 : 1;
+  }
+
+  win(board){
+
+    const parse = (int) => int === 1 ? 'red' : 'yellow';
+
+    if (board[0][0] && (
+      board[0][0] === board[0][1] && board[0][0] === board[0][2] ||
+      board[0][0] === board[1][0] && board[0][0] === board[2][0]
+    )) return `${parse(board[0][0])} won`;
+
+    if (board[1][1] && (
+      board[1][1] === board[0][0] && board[1][1] === board[2][2] ||
+      board[1][1] === board[0][1] && board[1][1] === board[2][1] ||
+      board[1][1] === board[0][2] && board[1][1] === board[2][0] ||
+      board[1][1] === board[1][0] && board[1][1] === board[1][2]
+    )) return `${parse(board[1][1])} won`;
+
+    if (board[2][2] && (
+      board[2][2] === board[1][2] && board[2][2] === board[0][2] ||
+      board[2][2] === board[2][1] && board[2][2] === board[2][0]
+    )) return `${parse(board[2][2])} won`;
+
+    for (const row of board) for (const col of row) if (!col) return '';
+    return 'draw';
   }
 };
